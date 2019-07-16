735807d20ce0e05f8c87e738c1e036c7c3a77125
PeterCapo <pokoth2009@gmail.com>
2019-07-16 14:39:05 +0000

    update tests

--- test_bucketlists.py
+++ test_bucketlists.py
@@ -6,68 +6,68 @@ from app import create_app, db
 
 
 class BucketlistTestCase(unittest.TestCase):
-    """This class represents the bucketlist test case"""
+    """This class represents the payment test case"""
 
     def setUp(self):
         """Define test variables and initialize app."""
         self.app = create_app(config_name="testing")
         self.client = self.app.test_client
-        self.bucketlist = {'name': 'Go to Borabora for vacation'}
+        self.payment = {'commonid': 'online'}
 
         # binds the app to the current context
         with self.app.app_context():
             # create all tables
             db.create_all()
 
-    def test_bucketlist_creation(self):
-        """Test API can create a bucketlist (POST request)"""
-        res = self.client().post('/bucketlists/', data=self.bucketlist)
+    def test_payment_creation(self):
+        """Test API can create a payment (POST request)"""
+        res = self.client().post('/api/payments/', data=self.payment)
         self.assertEqual(res.status_code, 201)
-        self.assertIn('Go to Borabora', str(res.data))
+        self.assertIn('online', str(res.data))
 
-    def test_api_can_get_all_bucketlists(self):
-        """Test API can get a bucketlist (GET request)."""
-        res = self.client().post('/bucketlists/', data=self.bucketlist)
+    def test_api_can_get_all_payments(self):
+        """Test API can get a payment (GET request)."""
+        res = self.client().post('/api/payments/', data=self.payment)
         self.assertEqual(res.status_code, 201)
-        res = self.client().get('/bucketlists/')
+        res = self.client().get('/api/payments/')
         self.assertEqual(res.status_code, 200)
-        self.assertIn('Go to Borabora', str(res.data))
+        self.assertIn('online', str(res.data))
 
-    def test_api_can_get_bucketlist_by_id(self):
-        """Test API can get a single bucketlist by using it's id."""
-        rv = self.client().post('/bucketlists/', data=self.bucketlist)
+    def test_api_can_get_payment_by_id(self):
+        """Test API can get a single payment by using it's id."""
+        rv = self.client().post('/api/payments/', data=self.payment)
         self.assertEqual(rv.status_code, 201)
         result_in_json = json.loads(rv.data.decode('utf-8').replace("'", "\""))
         result = self.client().get(
-            '/bucketlists/{}'.format(result_in_json['id']))
+            '/api/payments/{}'.format(result_in_json['id']))
         self.assertEqual(result.status_code, 200)
-        self.assertIn('Go to Borabora', str(result.data))
+        self.assertIn('online', str(result.data))
 
-    def test_bucketlist_can_be_edited(self):
-        """Test API can edit an existing bucketlist. (PUT request)"""
+    def test_payment_can_be_edited(self):
+        """Test API can edit an existing payment. (PUT request)"""
         rv = self.client().post(
-            '/bucketlists/',
-            data={'name': 'Eat, pray and love'})
+            '/api/payments/',
+            data={'commonid': 'onlinepaybill'})
         self.assertEqual(rv.status_code, 201)
         rv = self.client().put(
-            '/bucketlists/1',
+            '/api/payments/1',
             data={
-                "name": "Dont just eat, but also pray and love :-)"
+                "commonid": "online"
             })
         self.assertEqual(rv.status_code, 200)
-        results = self.client().get('/bucketlists/1')
-        self.assertIn('Dont just eat', str(results.data))
+        results = self.client().get('/api/payments/1')
+        self.assertIn('onlinepay', str(results.data))
 
-    def test_bucketlist_deletion(self):
-        """Test API can delete an existing bucketlist. (DELETE request)."""
+    def test_payment_deletion(self):
+        """Test API can delete an existing payment. (DELETE request)."""
         rv = self.client().post(
-            '/bucketlists/',
-            data={'name': 'Eat, pray and love'})
+            '/api/payments/',
+            data={'commonid': 'onlinepaybill'})
         self.assertEqual(rv.status_code, 201)
-        res = self.client().delete('/bucketlists/1')
+        res = self.client().delete('/api/payments/1')
         self.assertEqual(res.status_code, 200)
         # Test to see if it exists, should return a 404
-        result = self.client().get('/bucketlists/1')
+        result = self.client().get('/api/payments/1')
         self.assertEqual(result.status_code, 404)
 
     def tearDown(self):
